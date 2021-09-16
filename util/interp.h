959eb7b314dfefb5753acb5984f7752f036fb077
Sean Bartell <smbarte2@illinois.edu>
2021-09-16 10:50:42 -0500

    create Interpreter class
    This helps organize the code, and it will provide a way for alive-worker
    to retrieve the final state and override the way function arguments are
    generated.

--- util/interp.h
+++ util/interp.h
@@ -3,9 +3,51 @@
 // Copyright (c) 2018-present The Alive2 Authors.
 // Distributed under the MIT license that can be found in the LICENSE file.
 
-namespace IR { class Function; }
+#include <map>
+
+namespace IR {
+class BasicBlock;
+class Function;
+class Input;
+class Value;
+}
 
 namespace util {
+
+class ConcreteVal;
+
+class Interpreter {
+public:
+  Interpreter(IR::Function &f);
+  virtual ~Interpreter();
+  void step();
+  void run(unsigned instr_limit = 100);
+  virtual ConcreteVal *getInputValue(unsigned index, const IR::Input &input);
+
+  bool isReturned() const {
+    return !cur_block;
+  }
+
+  bool isUndefined() const {
+    return UB_flag;
+  }
+
+  bool isUnsupported() const {
+    return unsupported_flag;
+  }
+
+  bool isFinished() const {
+    return isReturned() || isUndefined() || isUnsupported();
+  }
+
+  std::map<const IR::Value *, ConcreteVal *> concrete_vals;
+  const IR::BasicBlock *pred_block = nullptr;
+  const IR::BasicBlock *cur_block = nullptr;
+  unsigned pos_in_block = 0;
+  bool UB_flag = false;
+  bool unsupported_flag = false;
+  ConcreteVal *return_value = nullptr;
+};
 
 void interp(IR::Function &f);
 
