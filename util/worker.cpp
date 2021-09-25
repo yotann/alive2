abf996487ba1d05e0c879410e9e51d82af7ff8dc
Sean Bartell <smbarte2@illinois.edu>
2021-09-25 13:43:56 -0500

    worker: include arbitrary uint64s in result
    The MemoDB server has been modified to support arbitrary uint64s.

--- util/worker.cpp
+++ util/worker.cpp
@@ -129,6 +129,9 @@ static ojson modelValToJSON(const IR::State &state, const smt::Model &m,
     int64_t i64;
     if (example.isInt(i64))
       return i64;
+    uint64_t u64;
+    if (example.isUInt(u64))
+      return u64;
     return storeAPIntAsByteString(bvToAPInt(example));
   } else if (type.isFloatType()) {
     smt::expr example = m.eval(val.value, true).float2BV();
